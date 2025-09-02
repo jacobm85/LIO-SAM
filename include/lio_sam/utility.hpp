28877ae8add7ef0548f078a1d2eaf643594b226e
jacobm85 <50577311+jacobm85@users.noreply.github.com>
2025-09-02 22:39:49 +0200

    Hesai support

--- include/lio_sam/utility.hpp
+++ include/lio_sam/utility.hpp
@@ -62,7 +62,7 @@ using namespace std;
 
 typedef pcl::PointXYZI PointType;
 
-enum class SensorType { VELODYNE, OUSTER, LIVOX };
+enum class SensorType { VELODYNE, OUSTER, LIVOX, HESAI };
 
 class ParamServer : public rclcpp::Node
 {
@@ -200,6 +200,10 @@ public:
         else if (sensorStr == "livox")
         {
             sensor = SensorType::LIVOX;
+        }
+        else if (sensorStr == "hesai")
+        {
+            sensor = SensorType::HESAI;
         }
         else
         {
