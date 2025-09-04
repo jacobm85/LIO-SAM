d1d3e4870354b99a5d71bc200b7d0d4138a58635
jacobm85 <50577311+jacobm85@users.noreply.github.com>
2025-09-04 21:09:01 +0200

    Update run.launch.py
    change for ugv xacro

--- launch/run.launch.py
+++ launch/run.launch.py
@@ -10,7 +10,7 @@ def generate_launch_description():
 
     share_dir = get_package_share_directory('lio_sam')
     parameter_file = LaunchConfiguration('params_file')
-    xacro_path = os.path.join(share_dir, 'config', 'robot.urdf.xacro')
+    xacro_path = os.path.join(share_dir, 'config', 'ugv.urdf.xacro')
     rviz_config_file = os.path.join(share_dir, 'config', 'rviz2.rviz')
 
     params_declare = DeclareLaunchArgument(
